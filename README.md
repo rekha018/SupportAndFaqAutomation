1b9bf55d54145a6bac94e414385bc59fad211955
rekha018 <rekhabiradar018@gmail.com>
2020-03-13 19:48:55 +0100

    Implement unused tests and add README file

---
+++ README.md
@@ -0,0 +1,27 @@
+# Wire Support and FAQ
+
+Automation tests for Wire support and FAQ page.
+
+## Test Scenarios
+
+1. Verify that main elements on Support and FAQ page are displayed
+
+2. Search results displayed should be relevant to search keyword
+
+3. Verify search functionality for no results
+
+## Prerequisites
+
+* Eclipse IDE
+* Selenium WebDriver
+* Chrome browser and chrome driver
+
+
+## Tech/framework used
+
+* Java
+* Cucumber
+* Maven
+
+## Clone
+Clone this repo using [SupportAndFaqAutomation](https://github.com/rekha018/SupportAndFaqAutomation.git)
--- src/test/java/stepDefinitions/StepDefsSupportAndFaqPage.java
+++ src/test/java/stepDefinitions/StepDefsSupportAndFaqPage.java
@@ -1,6 +1,5 @@
 package stepDefinitions;
 
-
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
@@ -15,27 +14,27 @@ public class StepDefsSupportAndFaqPage {
 	SupportAndFaqResultsPage supportAndFaqResultsPage = new SupportAndFaqResultsPage();
 
 	@Given("^I launch Chrome browser$")
-	public void i_launch_Chrome_browser() {
+	public void I_launch_Chrome_browser() {
 		supportAndFaqPage.launchBrowser();
 	}
 
 	@Given("^I go to the Wire Home Page$")
-	public void i_go_to_the_Wire_Home_Page() {
+	public void I_go_to_the_Wire_Home_Page() {
 		wireHomePage.openWireHomeURL();
 	}
 
 	@When("^I click on Support & FAQ element$")
-	public void i_click_on_Support_FAQ_element() {
+	public void I_click_on_Support_FAQ_element() {
 		wireHomePage.clickOnSupportFaqElement();
 	}
 
 	@Then("^I navigate to Support and FAQ page$")
-	public void i_navigate_to_Support_and_FAQ_page() {
+	public void I_navigate_to_Support_and_FAQ_page() {
 		supportAndFaqPage.checkSupportAndFaqPageIsDisplayed();
 	}
 
 	@Then("^I verify that the page displays the Support and FAQ title$")
-	public void i_verify_that_the_page_displays_the_Support_and_FAQ_title() {
+	public void I_verify_that_the_page_displays_the_Support_and_FAQ_title() {
 		supportAndFaqPage.checkSupportAndFaqPageTitleIsDisplayed();
 	}
 
@@ -80,29 +79,29 @@ public class StepDefsSupportAndFaqPage {
 	}
 
 	@Given("^I navigate to Support & FAQ page$")
-	public void i_navigate_to_Support_FAQ_page() {
+	public void I_navigate_to_Support_FAQ_page() {
 		supportAndFaqPage.openSupportAndFaqPage();
 	}
 
 	@When("^I search for a keyword 'call'$")
-	public void i_search_for_a_keyword_call() {
+	public void I_search_for_a_keyword_call() {
 		supportAndFaqResultsPage.searchKeyword("call");
 	}
 
 	@Then("^I verify that search results are displayed for a keyword 'Call'$")
-	public void i_verify_that_search_results_are_displayed_for_a_keyword_Call() {
+	public void I_verify_that_search_results_are_displayed_for_a_keyword_Call() {
 		supportAndFaqResultsPage.areSearchResultsDisplayed();
 	}
-
+	
 	@When("^I search for a keyword '\\*'$")
-	public void i_search_for_a_keyword() {
-		supportAndFaqResultsPage.searchKeyword("*");
+	public void I_search_for_a_keyword() {
+	  supportAndFaqResultsPage.searchKeyword("*");
 	}
 
 	@Then("^I verify that no results are displayed for a keyword '\\*'$")
-	public void i_verify_that_no_results_are_displayed_for_a_keyword() {
+	public void I_verify_that_no_results_are_displayed_for_a_keyword() {
 		supportAndFaqResultsPage.isNoSearchResultsTextDisplayed();
-		supportAndFaqResultsPage.closeBrowser();
+		supportAndFaqResultsPage.closeBrowser();   
 	}
-
+	
 }
